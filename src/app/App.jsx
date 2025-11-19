79933a15f7609e55cdad3205d3dbaea6f97600fc
songsoo <ssh102525@naver.com>
2025-11-19 16:43:27 +0900

    react router 및 react query 세팅

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Home from '../page/Home';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+    },
+]);
 
 export default function App() {
-    return (
-        <div>
-            
-        </div>
-    );
+    return <RouterProvider router={router} />;
 }
-
