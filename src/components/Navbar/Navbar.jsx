c0b41363fcda81a02eaa4fb2fbeeb96bbf7ba3c9
songsoo <ssh102525@naver.com>
2025-11-20 09:46:54 +0900

    style/Navbar css 추가

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import SearchBar from './SearchBar';
 import { useNavigate } from 'react-router';
 
 export default function Navbar() {
-
     return (
-        <div className="flex">
-            <HomeButton/>
+        <div className="flex h-16 w-full items-center justify-between">
+            <HomeButton />
             <SearchBar />
+            <p></p>
         </div>
     );
 }
