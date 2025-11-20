7368a0b40c3cf848bc371b1e9a1a146e79163e52
songsoo <ssh102525@naver.com>
2025-11-20 10:52:38 +0900

    feat/검색창 x버튼 추가

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -1,3 +1,4 @@
+import { RxCross2 } from 'react-icons/rx';
 import { BiSearch } from 'react-icons/bi';
 import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router';
@@ -18,19 +19,38 @@ export default function SearchBar() {
 
     return (
         <form onSubmit={handleSubmit} className="flex h-10 overflow-hidden">
-            <input
-                type="text"
-                placeholder="검색"
-                value={query}
-                ref={titleRef}
-                className={`w-96 rounded-l-3xl border py-2 pl-5 outline-none ${isInputFocused ? ' border-blue-700' : 'border-neutral-700'}`}
-                onChange={(e) => {
-                    setQuery(e.target.value);
-                }}
-                onFocus={() => setIsInputFocused(true)}
-                onBlur={() => setIsInputFocused(false)}
-            ></input>
-            <button className="flex w-16 items-center justify-center rounded-r-3xl border-y border-r border-neutral-700 bg-neutral-800 py-2 text-2xl hover:cursor-pointer">
+            <div
+                className={`relative flex w-96 items-center rounded-l-3xl border ${isInputFocused ? ' border-blue-700' : 'border-neutral-700'}`}
+            >
+                <input
+                    type="text"
+                    placeholder="검색"
+                    value={query}
+                    ref={titleRef}
+                    onChange={(e) => {
+                        setQuery(e.target.value);
+                    }}
+                    className="w-full py-2 pl-5 outline-none"
+                    onFocus={() => setIsInputFocused(true)}
+                    onBlur={() => setIsInputFocused(false)}
+                ></input>
+                {query.trim() !== '' && (
+                    <button
+                        className="mx-1 rounded-full p-1 text-xl hover:cursor-pointer hover:bg-neutral-500"
+                        onClick={() => {
+                            setQuery('');
+                            titleRef.current?.focus();
+                        }}
+                        type="button"
+                    >
+                        <RxCross2 />
+                    </button>
+                )}
+            </div>
+            <button
+                type="submit"
+                className="flex w-16 items-center justify-center rounded-r-3xl border-y border-r border-neutral-700 bg-neutral-800 py-2 text-2xl hover:cursor-pointer"
+            >
                 <BiSearch />
             </button>
         </form>
