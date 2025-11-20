afe41c71e1088b46e828cc1542e59796422c0789
songsoo <ssh102525@naver.com>
2025-11-20 09:57:28 +0900

    style/navbar css 추가

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,10 +5,13 @@ import { useNavigate } from 'react-router';
 
 export default function Navbar() {
     return (
-        <div className="flex h-16 w-full items-center justify-between">
-            <HomeButton />
-            <SearchBar />
-            <p></p>
-        </div>
+        <nav>
+            <div className="p-b-8 mt-2 flex h-16 w-full items-center justify-between">
+                <HomeButton />
+                <SearchBar />
+                <span></span>
+            </div>
+            <div className="mt-2 border-b border-neutral-600"></div>
+        </nav>
     );
 }
