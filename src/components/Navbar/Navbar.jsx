8188a81d7a0e5616b73fe5e93474a8dd8d75a658
songsoo <ssh102525@naver.com>
2025-11-21 20:24:31 +0900

    style/ video card 재사용을 위한 css 추가

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
                 <SearchBar />
                 <span></span>
             </div>
-            <div className="mt-1 border-b border-neutral-600"></div>
+            <div className="mt-1 mb-5 border-b border-neutral-600"></div>
         </nav>
     );
 }
--- src/components/Videos/VideoCard.jsx
+++ src/components/Videos/VideoCard.jsx
@@ -1,42 +1,62 @@
 import { colors } from '../../utils/video';
-import { useVideoCard } from '../../hooks/videoHooks';
+import { useVideoData } from '../../hooks/videoHooks';
 import { decodeHtml } from '../../utils/text';
 import { useNavigate } from 'react-router';
 
-export default function VideoCard({ item }) {
-    const { channelInfo, colorNum } = useVideoCard(item);
+export default function VideoCard({
+    item,
+    isVertical = true,
+    showChannelImg = true,
+    showHoverEffect = true,
+}) {
+    const { channelInfo, colorNum } = useVideoData(item.snippet.channelId, item.id.videoId);
     const navigate = useNavigate();
+    const css = [{ titleSize: 'text-sm' }, { titleSize: 'text-md' }];
 
     const handleClick = () => {
-        navigate(`videos/watch/${item.id.videoId}`);
+        navigate(`/videos/watch/${item.id.videoId}`);
     };
 
     return (
-        <div className="group relative h-fit w-full hover:cursor-pointer" onClick={handleClick}>
+        <div
+            className={`group relative h-fit w-full hover:cursor-pointer ${!isVertical && 'flex gap-3'}`}
+            onClick={handleClick}
+        >
             <img
                 src={item.snippet.thumbnails.high.url}
-                className="relative z-10 aspect-video w-full rounded-xl object-cover"
+                className={`relative z-10 aspect-video rounded-xl object-cover ${isVertical ? 'w-full mb-2' : 'w-40'}`}
             />
-            <div className="mt-3 flex gap-3">
-                <img
-                    className="mt-1 h-10 w-10 rounded-full"
-                    src={channelInfo?.thumbnails?.high?.url}
-                />
-                <div>
-                    <p className="line-clamp-2 font-extrabold break-all text-neutral-100">
+            <div className={`flex flex-1 gap-3`}>
+                {showChannelImg && (
+                    <img
+                        className="mt-1 h-10 w-10 rounded-full"
+                        src={channelInfo?.thumbnails?.high?.url}
+                    />
+                )}
+
+                <div >
+                    <p
+                        className={`line-clamp-2 font-extrabold break-all text-neutral-100 ${isVertical ? 'text-md' : 'text-sm'} `}
+                    >
                         {decodeHtml(item.snippet.title)}
                     </p>
-                    <p className="w-fit text-[0.85rem] font-medium text-neutral-400 hover:text-white">
+                    <p
+                        className={`w-fit font-medium text-neutral-400 ${isVertical ? 'text-[0.825rem] hover:text-white' : 'text-xs'}`}
+                    >
                         {item.snippet.channelTitle}
                     </p>
-                    <p className="text-[0.85rem] font-medium text-neutral-400">
+                    <p
+                        className={`${isVertical ? 'text-[0.85rem]' : 'text-xs'} font-medium text-neutral-400`}
+                    >
                         {item.snippet.publishedAt}
                     </p>
                 </div>
             </div>
-            <div
-                className={`pointer-events-none absolute top-1/2 left-1/2 h-full w-full -translate-1/2 rounded-xl ${colors[colorNum]} opacity-0 transition duration-400 ease-out group-hover:scale-107 group-hover:cursor-pointer group-hover:opacity-30`}
-            ></div>
+            {showHoverEffect && (
+                <div
+                    className={`pointer-events-none absolute top-1/2 left-1/2 h-full w-full -translate-1/2 rounded-xl ${colors[colorNum]} opacity-0 transition duration-400 ease-out group-hover:scale-107 group-hover:cursor-pointer group-hover:opacity-30`}
+                ></div>
+            )}
         </div>
     );
 }
