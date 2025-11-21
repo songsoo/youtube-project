3480eb9ec9d21e9f996918078f8c23e63f608764
songsoo <ssh102525@naver.com>
2025-11-21 14:08:46 +0900

    style/ index 최대 사이즈 조정 및 배경색 root에서 body로 이동

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar/Navbar';
 
 export default function Root() {
     return (
-        <div className="h-full w-full max-w-[90vw] flex-1 text-amber-50">
+        <div className="h-full w-full max-w-[80vw] flex-1 text-amber-50 min-[250rem]:w-[50vw]">
             <Navbar />
             <Outlet />
         </div>
