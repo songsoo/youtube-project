2ad79014c149f3e81ceef4a9c7867aef6421e12e
songsoo <ssh102525@naver.com>
2025-11-21 20:24:57 +0900

    feat/ 비디오 관련 훅 명칭 변경

--- src/hooks/videoHooks.js
+++ src/hooks/videoHooks.js
@@ -2,18 +2,18 @@ import { useQuery } from '@tanstack/react-query';
 import { useMemo } from 'react';
 import { getVideoIndex } from '../utils/video';
 
-export function useVideoCard(item) {
+export function useVideoData(channelId, videoId) {
     const { data: channelInfo } = useQuery({
-        queryKey: ['channelInfo', item.snippet.channelId],
+        queryKey: ['channelInfo', channelId],
         queryFn: () => {
-            return fetch(`data/channelInfo.json`)
+            return fetch(`/data/channelInfo.json`)
                 .then((response) => response.json())
-                .then((yeah) => {
-                    return yeah.items[0].snippet;
+                .then((data) => {
+                    return data.items[0].snippet;
                 });
         },
         staleTime: 1000 * 60 * 50,
     });
-    const colorNum = useMemo(() => getVideoIndex(item.id.videoId), [item.id.videoId]);
+    const colorNum = useMemo(() => getVideoIndex(videoId), [videoId]);
     return { channelInfo, colorNum };
 }
