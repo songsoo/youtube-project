1b8687d04fe35fee509628f23e9ef467b67cc237
songsoo <ssh102525@naver.com>
2025-11-25 20:08:19 +0900

    feat/ navbar에 z index 추가

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router';
 
 export default function Navbar() {
     return (
-        <nav>
+        <nav className='relative z-10'>
             <div className="p-b-8 flex h-16 w-full items-center justify-between">
                 <HomeButton />
                 <SearchBar />
