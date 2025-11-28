212f35082f4162baac3ee8e2aa5fc50e489b2fec
songsoo <ssh102525@naver.com>
2025-11-28 21:27:36 +0900

    feat: card 내부 스켈레톤 내용 추가

--- src/components/Videos/VideoCard.jsx
+++ src/components/Videos/VideoCard.jsx
@@ -46,9 +46,10 @@ export default function VideoCard({
                         <span className="invisible"></span>
                     </div>
                     <div className={`flex flex-1 gap-3`}>
-                        <div className="bg-skeleton mt-1 h-10 w-10 rounded-full" />
+                        {isVertical && <div className="bg-skeleton mt-1 h-10 w-10 rounded-full" />}
                         <div className={`flex flex-1 flex-col gap-2`}>
                             <div className={`bg-skeleton block h-5 w-full rounded-md`}></div>
+                            <div className={`bg-skeleton block h-3 w-1/3 rounded-md`}></div>
                             <div className={`bg-skeleton block h-3 w-1/3 rounded-md`}></div>
                         </div>
                     </div>
