2db4a274966cd8c73817d80e0da4d826c3e35ec4
songsoo <ssh102525@naver.com>
2025-12-02 19:00:05 +0900

    feat: 동영상 리스트를 API 도입

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
 import VideoList from '../components/Videos/VideoList';
 import { useParams } from 'react-router';
-import { useEffect } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { QueryClient, useQuery } from '@tanstack/react-query';
+import Youtube from '../utils/youtubeAPI';
 
 export default function Videos() {
     const { searchQuery } = useParams();
+    const queryClient = new QueryClient();
     const {
         isLoading,
         error,
         data: videos,
     } = useQuery({
-        queryKey: ['videos'],
-        queryFn: () => {
-            return fetch(`/data/listbykeyword.json`).then((response) => response.json());
+        queryKey: ['videoList', searchQuery ? searchQuery : 'popular'],
+        queryFn: async () => {
+            const youtube = new Youtube();
+            if (searchQuery) {
+                return await youtube.searchByKeyword(searchQuery);
+            } else {
+                return await youtube.getPopularVideos();
+            }
         },
         staleTime: 1000 * 60 * 50, //50분
+        onSuccess: (videos) => {
+            videos.forEach((video) => {
+                queryClient.setQueryData(['video', video.videoId], video);
+            });
+        },
     });
 
     return (
         <div className="flex">
-            <VideoList videos={videos} />
+            <VideoList videos={videos} isLoading={isLoading} />
         </div>
     );
 }
