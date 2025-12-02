79ac9601e5b27d28de41082c7949d2aa21c12706
songsoo <ssh102525@naver.com>
2025-12-02 19:00:20 +0900

    feat: 동영상 detail 페이지 API 도입

--- src/components/Videos/RelatedVideos.jsx
+++ src/components/Videos/RelatedVideos.jsx
@@ -1,26 +1,48 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, QueryClient } from '@tanstack/react-query';
 import VideoCard from './VideoCard';
+import Youtube from '../../utils/youtubeAPI';
+import VideoCardSkeleton from './VideoCardSkeleton';
 
 export default function RelatedVideos() {
+    const queryClient = new QueryClient();
     const {
         isLoading,
         error,
         data: videos,
     } = useQuery({
-        queryKey: ['videos'],
-        queryFn: () => {
-            return fetch(`/data/listbykeyword.json`).then((response) => response.json());
+        queryKey: ['videos', 'popular'],
+        queryFn: async () => {
+            const youtube = new Youtube();
+            return await youtube.getPopularVideos();
         },
         staleTime: 1000 * 60 * 50, //50분
+        onSuccess: (videos) => {
+            videos.forEach((video) => {
+                queryClient.setQueryData(['video', video.videoId], video);
+            });
+        },
     });
 
-    return (
+    return isLoading ? (
         <>
             <nav className="flex shrink-0 basis-96 flex-col gap-2 lg:hidden">
-                {videos?.items?.map((item, index) => (
+                {Array.from({ length: 12 }).map((_, i) => (
+                    <VideoCardSkeleton isVertical={true} key={i} />
+                ))}
+            </nav>
+            <nav className="hidden shrink-0 basis-96 flex-col gap-2 lg:flex">
+                {Array.from({ length: 12 }).map((_, i) => (
+                    <VideoCardSkeleton isVertical={false} key={i} />
+                ))}
+            </nav>
+        </>
+    ) : (
+        <>
+            <nav className="flex shrink-0 basis-96 flex-col gap-2 lg:hidden">
+                {videos.map((item, index) => (
                     <VideoCard
-                        item={item}
                         key={index}
+                        video={item}
                         isVertical={true}
                         showChannelImg={true}
                         showHoverEffect={true}
@@ -28,9 +50,9 @@ export default function RelatedVideos() {
                 ))}
             </nav>
             <nav className="hidden shrink-0 basis-96 flex-col gap-2 lg:flex">
-                {videos?.items?.map((item, index) => (
+                {videos.map((item, index) => (
                     <VideoCard
-                        item={item}
+                        video={item}
                         key={index}
                         isVertical={false}
                         showChannelImg={false}
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -2,29 +2,35 @@ import { useParams } from 'react-router';
 import VideoPlayer from '../components/Videos/VideoPlayer';
 import { useQuery } from '@tanstack/react-query';
 import RelatedVideos from '../components/Videos/RelatedVideos';
+import Youtube from '../utils/youtubeAPI';
+import VideoPlayerSkeleton from '../components/Videos/VideoPlayerSkeleton';
 
 export default function VideoDetail() {
     const { videoId } = useParams();
     const {
         isLoading,
         error,
-        data: videoDetail,
+        data: video,
     } = useQuery({
-        queryKey: ['videoDetail', videoId],
-        queryFn: () => {
-            const youtube = new Youtube();
-            return youtube.getVideoInfo(videoId);
+        queryKey: ['video', videoId],
+        queryFn: async () => {
+            try {
+                const youtube = new Youtube();
+                return youtube.getVideoChannelInfo(videoId);
+            } catch (e) {
+                console.log(e);
+            }
         },
         staleTime: 1000 * 60 * 50, //50분
     });
 
     return (
         <main className="flex flex-col gap-5 lg:flex-row">
-            <VideoPlayer
-                videoId={videoId}
-                videoDetail={videoDetail?.items[0]}
-                isLoading={isLoading}
-            />
+            {isLoading ? (
+                <VideoPlayerSkeleton />
+            ) : (
+                <VideoPlayer videoId={videoId} video={video} isLoading={isLoading} />
+            )}
             <RelatedVideos />
         </main>
     );
