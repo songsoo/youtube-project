c97043707152b641db452df20dcd2193e914ceff
songsoo <ssh102525@naver.com>
2025-12-03 21:23:01 +0900

    feat: API 할당량 초과 상태에 따른 페이지 추가

--- src/components/Videos/VideoList.jsx
+++ src/components/Videos/VideoList.jsx
@@ -1,12 +1,19 @@
+import Error from '../Common/Error';
 import VideoCard from './VideoCard';
 import VideoCardSkeleton from './VideoCardSkeleton';
 
-export default function VideoList({ videos, isLoading }) {
+export default function VideoList({ videos, isLoading, error }) {
     return (
-        <div className="grid w-full grid-cols-1 gap-4 min-[50rem]:grid-cols-2 min-[85rem]:grid-cols-3 min-[110rem]:grid-cols-4">
-            {isLoading
-                ? Array.from({ length: 24 }).map((_, i) => <VideoCardSkeleton isVertical={true} />)
-                : videos?.map((video, index) => <VideoCard video={video} key={video.videoId} />)}
+        <div className="grid w-full h-full grid-cols-1 gap-4 min-[50rem]:grid-cols-2 min-[85rem]:grid-cols-3 min-[110rem]:grid-cols-4">
+            {isLoading ? (
+                Array.from({ length: 24 }).map((_, i) => (
+                    <VideoCardSkeleton isVertical={true} key={i} />
+                ))
+            ) : error ? (
+                <Error ErrorClass="w-full h-full" />
+            ) : (
+                videos?.map((video, index) => <VideoCard video={video} key={video.videoId} />)
+            )}
         </div>
     );
 }
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -4,13 +4,18 @@ import { useQuery } from '@tanstack/react-query';
 import RelatedVideos from '../components/Videos/RelatedVideos';
 import Youtube from '../utils/youtubeAPI';
 import VideoPlayerSkeleton from '../components/Videos/VideoPlayerSkeleton';
+import Error from '../components/Common/Error';
+import { useEffect } from 'react';
+import { useApiAvailable } from '../context/ApiCheckContextProvider';
 
 export default function VideoDetail() {
+    const { controlIsApiAvailable } = useApiAvailable();
     const { videoId } = useParams();
     const {
         isLoading,
         error,
         data: video,
+        status,
     } = useQuery({
         queryKey: ['video', videoId],
         queryFn: async () => {
@@ -24,10 +29,22 @@ export default function VideoDetail() {
         staleTime: 1000 * 60 * 50, //50분
     });
 
+    useEffect(() => {
+        if (status === 'error') {
+            controlIsApiAvailable(false);
+            // const reason = error?.response?.data?.error?.errors?.[0]?.reason;
+            // if (reason === 'quotaExceeded') {
+            //     controlIsApiAvailable(false);
+            // }
+        }
+    }, [status]);
+
     return (
-        <main className="flex flex-col gap-5 lg:flex-row">
+        <main className="flex w-full flex-col gap-5 lg:flex-row">
             {isLoading ? (
                 <VideoPlayerSkeleton />
+            ) : error ? (
+                <Error ErrorClass="relative w-full flex-1 shrink" />
             ) : (
                 <VideoPlayer videoId={videoId} video={video} isLoading={isLoading} />
             )}
