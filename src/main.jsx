9526bbb33970461d804b2dd46a9c2c8763d2479f
songsoo <ssh102525@naver.com>
2025-12-04 20:48:27 +0900

    chore: react-query-devtool 경로 변경

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app/App';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ApiCheckContextProvider } from './context/ApiCheckContextProvider';
+import { ReactQueryDevtools } from 'react-query-devtools';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
